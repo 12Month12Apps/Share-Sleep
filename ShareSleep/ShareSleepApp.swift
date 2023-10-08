1f64018fbe665c22f3d98a9b81dc5ceb95c1ed0c
Veit Progl <veit-progl@t-online.de>
2023-10-08 10:00:25 +0200

    Initial Commit

---
+++ ShareSleep/ShareSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareSleepApp.swift
+//  ShareSleep
+//
+//  Created by Veit Progl on 08.10.23.
+//
+
+import SwiftUI
+
+@main
+struct ShareSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
